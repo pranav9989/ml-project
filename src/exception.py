9d63d9c0cbcb2d0e55335ea5cbe41e8016251909
vanarp99 <2022.pranav.raghuvanshi@ves.ac.in>
2025-05-24 11:23:10 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,17 @@
+import sys
+
+def error_msg_detail(error, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name= exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{}] at line number [{}] error message [{}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+class CustomeException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_msg_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
